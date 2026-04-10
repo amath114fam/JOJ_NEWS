6606ac3923abe6b8d94091764eefc2d2a1f44ecd
bayebasse <bayebassdieye@gmail.com>
2026-04-10 13:50:27 +0200

    Réinitialisation Mot de Passe

--- news/urls.py
+++ news/urls.py
@@ -10,6 +10,22 @@ urlpatterns = [
     path('inscription/', inscription, name='inscription'),
     path('connexion/', auth_views.LoginView.as_view(template_name='connexion.html'), name='connexion'),
     path('deconnexion/', auth_views.LogoutView.as_view(next_page='home'), name='deconnexion'),
+
+    path('motdepasseOUBLIER/',
+         auth_views.PasswordResetView.as_view(template_name='motpasseOublier.html'),
+         name='password_reset'),
+
+    path('motdepasse-envoyer/',
+         auth_views.PasswordResetDoneView.as_view(template_name='mailmotdepasse.html'),
+         name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='nouveaumotpasse.html'),
+         name='password_reset_confirm'),
+
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(template_name='motpasseModifier.html'),
+         name='password_reset_complete'),
+      
     path('liste/', views.liste_article , name='liste_article'),
     path('Article/<int:id>/', views.detail_article , name='detail_article'),
     path('commentaire/<int:pk>/modifier/', CommentaireUpdateView.as_view(), name='modifier_commentaire'),
